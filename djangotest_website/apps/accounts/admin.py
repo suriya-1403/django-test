0112a56ee556f7f2b6f82449cd90132db5070947
suriyakrishnan <suriya@outlook.in>
2020-09-29 15:09:22 +0530

    completed creatation of profile with database

--- djangotest_website/apps/accounts/admin.py
+++ djangotest_website/apps/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import userPersona, UserProfile, UserInterest
 # Register your models here.
+admin.site.register(userPersona)
+admin.site.register(UserProfile)
+admin.site.register(UserInterest)
